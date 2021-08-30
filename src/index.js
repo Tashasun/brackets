26017c8cc5bb4b197e1f67952a57edf8b7754968
tashasun <natalia.limbakh@gmail.com>
2021-08-30 19:05:45 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  // const check = (str, bracketsConfig) => {
+  l = bracketsConfig.length;
+  long = str.length;
+  for (let k = 0; k < long; k++) {
+    str = str.replace(" ", "");
+    for (let i = 0; i < l; i++) {
+      let left = "";
+      let right = "";
+      change = `${bracketsConfig[i][0]}` + `${bracketsConfig[i][1]}`;
+      str = str.replace(change, "");
+      // console.log(change,str);
+    }
+  }
+  if (str == "") {
+    return true;
+  }
+  return false;
+};
+
+// console.log(
+//   check("{,[     ]()  }", [['(', ')'], ['[', ']']])
+// );
